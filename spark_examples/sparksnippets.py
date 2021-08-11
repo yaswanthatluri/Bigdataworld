4d870b06fb005270600dee42a44b77c852408b66
Yaswanth Atluri <47100663+yaswanthatluri@users.noreply.github.com>
2021-08-11 12:01:11 -0700

    Update sparksnippets.py

--- spark_examples/sparksnippets.py
+++ spark_examples/sparksnippets.py
@@ -1,1 +1,3 @@
-
+#renaming multiple columns in spark
+df1 = spark.read.format("csv").option("header", "true").load(input_path)
+df1 = df1.withColumnRenamed("Semiotic Class","semioticclass").withColumnRenamed("Input Token","inputtoken").withColumnRenamed("Output Token","outputtoken")
